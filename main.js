6b43c9471ea4ed8afe6e9a7c0746a0b74dc52556
Piaashi <75792228+Piaashi@users.noreply.github.com>
2021-06-26 15:36:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function Check1()
+{
+    document.getElementById("A1").innerHTML="plant, forest, landscape";
+    document.getElementById("M1").innerHTML="tree, trees"
+}
+
+
+function Check2()
+{
+    document.getElementById("A2").innerHTML="watch, handwatch, wristband";
+    document.getElementById("M2").innerHTML="clock, time, bracelet, wall clock"
+}
+
+
+function Check3()
+{
+    document.getElementById("A2").innerHTML="gadget, person, organism, machine, electronic-device";
+    document.getElementById("M2").innerHTML="robot,alien,statue"
+}
